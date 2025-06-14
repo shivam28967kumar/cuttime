46459688cd51cb8542b0a8f9d68a1e85ac8d3a0c
shivam28967kumar <shivam0469719@gmail.com>
2025-06-14 23:34:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const nodemailer = require('nodemailer');
 const fs = require('fs');
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('✅ Connected to MongoDB'))
 .catch((err) => {
   console.error('❌ MongoDB connection error:', err);
